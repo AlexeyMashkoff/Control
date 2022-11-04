1216e7e3ee8bea7dee4e3a09694c5e63880eb8d7
Алексей Машков <AlexeyMashkoff@yandex.ru>
2022-11-04 22:19:08 +0300

    v3 realized task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array = Array1();
+string[] result = Less(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] Less(string[] input, int n) 
+{
+    string[] output = new string[Count(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int Count(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+string[] Array1() 
+{
+    Console.Write("Введите любое значение через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
